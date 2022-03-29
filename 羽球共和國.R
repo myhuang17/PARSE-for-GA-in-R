ff5a003b187fddfad1ed41b6c2e6ac905669b7a3
myhuang17 <98328497+myhuang17@users.noreply.github.com>
2022-03-29 12:36:10 +0800

    Update 羽球共和國.R

--- 羽球共和國.R
+++ 羽球共和國.R
@@ -1,12 +1,31 @@
 library(rvest)
-html = read_html("http://www.badmintonrepublic.com/phpbb3/viewforum.php?f=17&sid=df582166a8444e7bd7c7e39c772666e5")
-title = html_nodes(html, "a.topictitle")
-tt = html_text(title)#文章名
-zz = html_attrs(title)#有連結，href
-lk = NULL
-for (i in 1:length(zz)) {
-  pp = as.character(zz[[i]][1])
-  pp = substr(pp,2,nchar(pp))
-  lk[i] = paste0("http://www.badmintonrepublic.com/phpbb3", pp)
+cawler = function(link){
+  html = read_html(link)
+  title = html_nodes(html, "a.topictitle")
+  tt = html_text(title)#文章名
+  tt = tt[4:length(tt)]
+  zz = html_attrs(title)#有連結，href
+  lk = NULL
+  for (i in 4:length(zz)) {
+    pp = as.character(zz[[i]][1])
+    pp = substr(pp,2,nchar(pp))
+    lk[i] = paste0("http://www.badmintonrepublic.com/phpbb3", pp)
+  }
+  lk = lk[4:length(lk)]
+  tab = cbind(tt,lk)
+  return(tab)
 }
-table = cbind(tt,lk)
+
+
+table = NULL
+for (x in 0:1) {
+  if(x == 0){
+    html_t = "http://www.badmintonrepublic.com/phpbb3/viewforum.php?f=17"
+  }
+  else{
+    html_t = paste0("http://www.badmintonrepublic.com/phpbb3/viewforum.php?f=17","&start=",x*36)
+  }
+  #cawler
+  tb = cawler(html_t)
+  table = rbind(table,tb)
+}
