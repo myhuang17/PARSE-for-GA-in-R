5ed8cd3239050844169bca95010debbc08ad74b7
myhuang17 <98328497+myhuang17@users.noreply.github.com>
2022-04-03 18:08:29 +0800

    Update 中羽在線.R

--- 中羽在線.R
+++ 中羽在線.R
@@ -4,31 +4,38 @@ library(stringi)
 #=====================爬連結區__爬各廠
 rt1 = NULL
 rt2 = NULL
-for(i in 1:5){
-  link = paste0("https://www.badmintoncn.com/cbo_eq/list.php?brand=0&sort=0&class=1&fee=&name=&order=buy&page=",i)
-  html = read_html(link)
-  tt = html_nodes(html, "a.listName")
-  title = html_text(tt)
-  title = gsub("\n","",title)#標題
-  lk = html_attr(tt, "href")
-  lk_a = substr(lk, 1, 12)
-  lk_b = gsub(lk_a[1], "", lk)
-  lk = paste0("https://www.badmintoncn.com", lk_a, "_specs", lk_b)#連結
-  pt = html_nodes(html, "span.right")
-  pt = html_text(pt)
-  pt = pt[3:32]
-  pt = gsub("\n","",pt)#評分
-  rt = html_nodes(html, "span.smalltext")
-  rt = html_text(rt)[2:31]
-  rt = strsplit(rt,"；")
-  for(i in 1:length(rt)){
-    rt1[i] = rt[[i]][1]#想要
-    rt2[i] = rt[[i]][2]#用過
+big_table = NULL
+dh = c(1, 2, 13, 22)
+for (zx in 1:length(dh)) {
+  for(pg in 1:3){
+    link = paste0("https://www.badmintoncn.com/cbo_eq/list.php?brand=", dh[zx],"&sort=0&class=1&fee=&name=&order=buy&page=", pg)
+    html = read_html(link)
+    html = html_nodes(html, "div.list")
+    tt = html_nodes(html, "a.listName")
+    title = html_text(tt)
+    title = gsub("\n","",title)#標題
+    lk = html_attr(tt, "href")
+    lk_a = substr(lk, 1, 12)
+    lk_b = gsub(lk_a[1], "", lk)
+    lk = paste0("https://www.badmintoncn.com", lk_a, "_specs", lk_b)#連結
+    pt = html_nodes(html, "span.right")
+    pt = html_text(pt)
+    pt = pt[3:32]
+    pt = gsub("\n","",pt)#評分
+    rt = html_nodes(html, "span.smalltext")
+    rt = html_text(rt)[2:31]
+    rt = strsplit(rt,"；")
+    for(i in 1:length(rt)){
+      rt1[i] = rt[[i]][1]#想要
+      rt2[i] = rt[[i]][2]#用過
+    }
+    rt1 = gsub("人想要","",rt1)#想要
+    rt2 = gsub("人用过","",rt2)#用過
+    table = cbind(title, lk, pt, rt1, rt2)
   }
-  rt1 = gsub("人想要","",rt1)#想要
-  rt2 = gsub("人用过","",rt2)#用過
-  table = cbind(title, lk, pt, rt1, rt2)
+  big_table = rbind(big_table, table)
 }
+
 #==========================================相關資訊
 #基礎資料
 infos = function(link){
@@ -49,3 +56,12 @@ infos = function(link){
   return(pai)
 }
 
+
+#==================================MAIN
+btabo = NULL
+for (x in 1:length(big_table[,1])) {
+  binfo = infos(big_table[x,2])
+  btabo = rbind(btabo, binfo)
+}
+bx = cbind(big_table,btabo)
+write.csv(ans, file = 'C:\\Users\\user\\Desktop\\DATA_中羽在線.csv')
